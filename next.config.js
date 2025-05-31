ba14c42430400034c9ad60397b4ab46f1586bf54
xuke.fatdoge <xuke.fatdoge@bytedance.com>
2025-05-31 19:43:42 +0800

    feat: pwa

--- next.config.js
+++ next.config.js
@@ -1,11 +1,17 @@
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true
+});
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig = withPWA({
   // 移除或注释掉 output: 'export' 行
   // output: 'export',
   eslint: {
     ignoreDuringBuilds: true,
   },
   images: { unoptimized: true },
-};
+});
 
 module.exports = nextConfig;
